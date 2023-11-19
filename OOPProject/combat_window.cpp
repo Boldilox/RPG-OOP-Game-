87befd69b015924400b85d29533ef7ea0ca8bafb
Ahsan10426 <77637759+Ahsan10426@users.noreply.github.com>
2023-11-19 14:53:09 +0500

    commented this file, to make the rest of the code work.

--- OOPProject/combat_window.cpp
+++ OOPProject/combat_window.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
+// #include <iostream>
+// #include <SFML/Graphics.hpp>
 
-int main() {
-    // Create a window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Turn-Based RPG Game - Combat System");
+// int main() {
+//     // Create a window
+//     sf::RenderWindow window(sf::VideoMode(800, 600), "Turn-Based RPG Game - Combat System");
 
-    // Load textures for knight and enemy
-    sf::Texture knightTexture, enemyTexture;
-    if (!knightTexture.loadFromFile("knight.png")) {
-        std::cerr << "Failed to load knight texture" << std::endl;
-        return -1;
-    }
-    if (!enemyTexture.loadFromFile("enemy.png")) {
-        std::cerr << "Failed to load enemy texture" << std::endl;
-        return -1;
-    }
+//     // Load textures for knight and enemy
+//     sf::Texture knightTexture, enemyTexture;
+//     if (!knightTexture.loadFromFile("knight.png")) {
+//         std::cerr << "Failed to load knight texture" << std::endl;
+//         return -1;
+//     }
+//     if (!enemyTexture.loadFromFile("enemy.png")) {
+//         std::cerr << "Failed to load enemy texture" << std::endl;
+//         return -1;
+//     }
 
-    // Create sprites for knight and enemy
-    sf::Sprite knightSprite(knightTexture), enemySprite(enemyTexture);
+//     // Create sprites for knight and enemy
+//     sf::Sprite knightSprite(knightTexture), enemySprite(enemyTexture);
 
-    // Positioning the knight and enemy
-    knightSprite.setPosition(100, 300); // Knight's position
-    enemySprite.setPosition(600, 300);  // Enemy's position
+//     // Positioning the knight and enemy
+//     knightSprite.setPosition(100, 300); // Knight's position
+//     enemySprite.setPosition(600, 300);  // Enemy's position
 
-    // Game loop
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+//     // Game loop
+//     while (window.isOpen()) {
+//         sf::Event event;
+//         while (window.pollEvent(event)) {
+//             if (event.type == sf::Event::Closed)
+//                 window.close();
+//         }
 
-        window.clear();
-        window.draw(knightSprite); // Draw the knight
-        window.draw(enemySprite);  // Draw the enemy
-        window.display();
-    }
+//         window.clear();
+//         window.draw(knightSprite); // Draw the knight
+//         window.draw(enemySprite);  // Draw the enemy
+//         window.display();
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
