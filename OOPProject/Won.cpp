74b41ea11fae70a77ccf1abf103f02de45a28965
Ahsan10426 <77637759+Ahsan10426@users.noreply.github.com>
2023-11-30 10:51:35 +0500

    corrected the empty spaces

--- OOPProject/Won.cpp
+++ OOPProject/Won.cpp
@@ -1,3 +1,3 @@
 #include "Won.hpp"
 
-Won::Won(float p_x, float p_y, SDL_Texture* p_tex) : Entity(p_x, p_y, p_tex) {}
+Won::Won(float p_x, float p_y, SDL_Texture* p_tex) : Entity(p_x,p_y,p_tex) {}
--- OOPProject/Won.hpp
+++ OOPProject/Won.hpp
@@ -1,11 +1,9 @@
 #pragma once
-
 #include "Entity.hpp"
 #include "RenderWindow.hpp"
 
 class Won : public Entity {
 public:
     Won(float p_x, float p_y, SDL_Texture* p_tex);
-    
-   
+
 };
